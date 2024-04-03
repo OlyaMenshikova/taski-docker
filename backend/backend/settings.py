5058065ac5c65f9d9cd4f08f1c0694cb5961e8e4
Olya Menshikova <menshikova725@gmail.com>
2024-04-03 12:46:12 +0700

    Update settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = bool(os.getenv('DEBUG'))
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = ['crazy.hopto.org', '127.0.0.1']
 
